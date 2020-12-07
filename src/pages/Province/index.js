7b19a41a8bf5aaf3ee51cd24de6bf8bb65d2e369
fadiojody <911.jodyjoseph@gmail.com>
2020-12-07 20:26:23 +0700

    6th commit Memasukkan component Province dan insert data API

--- src/pages/Province/index.js
+++ src/pages/Province/index.js
@@ -3,7 +3,7 @@ import ProvinceCard from './ProvinceCard.js';
 
 const Province = () => {
     return (
-        <div>
+        <div className = "ProvinceText">
             <h1>Indonesia Province Case</h1>
             <ProvinceCard />
         </div>
